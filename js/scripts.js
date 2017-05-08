40a8fd380c315603291f112cdbb712bc1cbd0945
Patryk Nowak <patnow92@wp.pl>
2017-05-08 11:48:07 +0200

    dupa

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var a = prompt('wpisz wartość a');
+var	b = prompt('wpisz wartość b');
+var	value = (a * a) + (2 * a * b) - (b * b);
+ 
+if ( value > 0 ) {
+	console.log('wynik dodatni');
+} else if ( value < 0 ) {
+	console.log('wynik ujemny');
+} else if ( value == 0 ) {
+	console.log('wynik zerowy');
+ }
